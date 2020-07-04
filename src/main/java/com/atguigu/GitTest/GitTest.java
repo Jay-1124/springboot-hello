d05892b8beee2fb92e3468ec0abb99dbe9570fc4
Jay <495436917@qq.com>
2020-07-04 14:43:39 +0800

    git练习

---
+++ src/main/java/com/atguigu/GitTest/GitTest.java
@@ -0,0 +1,7 @@
+package com.atguigu.GitTest;
+
+public class GitTest {
+    public void test01(){
+        System.out.println("222222");
+    }
+}
--- src/test/java/GitTest.java
+++
@@ -1,5 +0,0 @@
-public class GitTest {
-    public void test01(){
-        System.out.println(123456);
-    }
-}
